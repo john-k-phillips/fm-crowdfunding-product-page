4f95516e9cdd8ae4c52b3af556657d7d07637a5d
John Phillips <johnphillips.dev@gmail.com>
2021-11-13 21:38:14 +0000

    Button now accessible.

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ let isOpen = true;
 const toggleMenu = () => {
     const dropdownList = document.querySelector('.dropdown-list');
     const hamburgerIcon = document.querySelector('.hamburger-icon');
+    const hamburgerButton = document.querySelector('.hamburger-btn');
 
     if (isOpen) {
         dropdownList.style.display = 'block';
         hamburgerIcon.src = './images/icon-close-menu.svg';
+        hamburgerButton.ariaExpanded = "true";
         setTimeout(() => {
             dropdownList.style.opacity = 1;
         }, 200);
     } else {
         dropdownList.style.opacity = 0;
         hamburgerIcon.src = './images/icon-hamburger.svg';
+        hamburgerButton.ariaExpanded = "false";
         setTimeout(() => {
             dropdownList.style.display = 'none';
         }, 200);
